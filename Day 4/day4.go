f701974dd67a53cd4c004275ba3d2e3f6d50ddb2
Salvador Vincent Javier <slvdrvncntjvr@gmail.com>
2026-01-04 20:37:05 +0800

    some strings and variable declaring

--- Day 4/day4.go
+++ Day 4/day4.go
@@ -13,4 +13,20 @@ func main() {
 	fmt.Println(x)
 	fmt.Println(z)
 	fmt.Println(x+z)
+
+	// the above is a wrong code
+	// you declare using :=  (that is shorthand decalaration where go automatically assumes the type of the data)
+	// := can only be used inside functions while var can be used anywhere
+	// := must assign a value, while var can declare without value
+
+	name := "Vincent"
+	age := 18
+	var nameclone string = "Tnecniv"
+	var nameclone2 string = "vince"
+	// this doesnt work nameclone3 = "tete"
+	var nameclone3 = "hahah"
+
+	fmt.Println(name, age, nameclone, nameclone2, nameclone3)
+	fmt.Println(name, age, nameclone, nameclone2+nameclone3) // ooh so add concatenates the screen
+
 }
